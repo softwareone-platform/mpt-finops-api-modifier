2a3c6bda0f31abbe8a20c7d1c2c09f8f9094616a
Antonio Di Mariano <antonio.dimariano@gmail.com>
2024-11-14 16:28:02 +0100

    Fix missing fields

--- app/invitations/model.py
+++ app/invitations/model.py
@@ -2,16 +2,16 @@ from pydantic import BaseModel, EmailStr
 
 
 class CreateInvitationData(BaseModel):
-    email: EmailStr
+    invited_email: EmailStr
     display_name: str
     organization_id: str
     model_config = {
         "json_schema_extra" :{
             "example" : [
                 {
-                   "id" : "1001-10010-1001-1001",
-                    "email" : "peter.parker@spiderman.com",
-                     "org_id" : "abc-1010-100-10"
+                    "invited_email" : "peter.parker@spiderman.com",
+                    "display_name" : "Peter Parker",
+                    "organization_id" : "abc-1010-100-10",
                 }
             ]
         }
@@ -23,42 +23,20 @@ class InvitationResponse(BaseModel):
     email: EmailStr
     display_name: str
     organization_id: str
+
     model_config = {
         "json_schema_extra" :{
             "example" : [
                 {
                     "id" : "1001-10010-1001-1001",
-                    "email" : "peter.parker@spiderman.com",
-                    "org_id" : "abc-1010-100-10"
+                    "invited_email" : "peter.parker@spiderman.com",
+                    "display_name" : "Peter Parker",
+                    "organization_id" : "abc-1010-100-10",
+                    "token" : "base64-token",
+                    "created_by_user_id": "1010-1001-10010",
+                    "created_at" :"2024-11-14:11:10:10",
+                    "expiration_date": "2024-11-21:11:10:10"
                 }
             ]
         }
     }
-
-class CreateInvitationResponse(BaseModel):
-    display_name: str
-    email: EmailStr
-    org_id: str
-
-
-    # class Config:
-    #     schema_extra = {
-    #         "example": {
-    #             "display_name": "your friendly neighborhood spider-man",
-    #             "email": "peter.parker@spider.com",
-    #             "org_id": "ABC-1223-ABC-123"
-    #             }
-    #     }
-
-class CheckInvitationResponse(BaseModel):
-    invited: bool
-    email: EmailStr
-
-
-    # class Config:
-    #     schema_extra = {
-    #         "example": {
-    #             "invited": True,
-    #             "email": "peter.parker@spider.com"
-    #         }
-    #     }
