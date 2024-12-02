4288fd8fd9ba4708a9d11933a29e7104646e9e02
Francesco Faraone <francesco.faraone@softwareone.com>
2024-12-02 13:29:15 +0100

    Fix licensor name

--- app/users/api.py
+++ app/users/api.py
@@ -16,28 +16,33 @@ router = APIRouter()
 @router.post(
     path="",
     status_code=http_status.HTTP_201_CREATED,
-    dependencies=[Depends(JWTBearer())]
+    dependencies=[Depends(JWTBearer())],
 )
-async def create_user(
-        data: CreateUserData,
-        user_api: OptScaleUserAPI = Depends()
-):
+async def create_user(data: CreateUserData, user_api: OptScaleUserAPI = Depends()):
     try:
-        response = await user_api.create_user(email=data.email,
-                                              display_name=data.display_name,
-                                              password=data.password)
+        response = await user_api.create_user(
+            email=data.email, display_name=data.display_name, password=data.password
+        )
         if response.get("error"):
             logger.warning(f"Failed to create user with email: {data.email}")
-            raise create_error_response(status_code=response.get("status_code",
-                                                                 http_status.HTTP_403_FORBIDDEN),
-                                        title="Cannot create the user",
-                                        errors={"reason": response.get("data", {}).get("error", {}).
-                                        get("reason", "")})
-        return JSONResponse(status_code=response.get("status_code", 200),
-                            content=response.get("data", {}))
+            raise create_error_response(
+                status_code=response.get("status_code", http_status.HTTP_403_FORBIDDEN),
+                title="Cannot create the user",
+                errors={
+                    "reason": response.get("data", {})
+                    .get("error", {})
+                    .get("reason", "")
+                },
+            )
+        return JSONResponse(
+            status_code=response.get("status_code", 200),
+            content=response.get("data", {}),
+        )
 
     except Exception as error:
         logger.error(f"Exception occurred during user creation: {error}")
-        raise create_error_response(status_code=http_status.HTTP_403_FORBIDDEN,
-                                    title="An exception occurred",
-                                    errors={"exception": str(error)})
+        raise create_error_response(
+            status_code=http_status.HTTP_403_FORBIDDEN,
+            title="An exception occurred",
+            errors={"exception": str(error)},
+        )
